a6fa4d0af9d7501368e4865bf65766fb31d24a0b
Maksym Kletsman <mkletsm1@stu.vistula.edu.pl>
2025-12-15 15:12:05 +0100

    Initial commit

---
+++ src/main/java/com/example/task1/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.task1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController
+{
+
+    @GetMapping("/")
+    @ResponseBody
+     public String hello()
+     {
+         return "Hello Spring Boot";
+    }
+
+    @GetMapping("/greeting")
+    public String greeting(Model model)
+     {
+        model.addAttribute("message", "Hello from  Gomer");
+        return "greeting";
+    }
+}
